4dbbf3ba0736003c4425a09a312808be67bcb221
Jonas <jonasonias@gmail.com>
2024-05-22 11:02:29 -0300

    sessin backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,12 +3,25 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = require('./models/User'); // Importar modelo de usuário
+const session = require('express-session');
+const crypto = require('crypto');
 const { connectToDatabase } = require('./db'); // Importar função de conexão
 
 const app = express();
 
 app.use(cors()); // Permite todas as origens
 app.use(express.json());
+
+// Gerar uma chave secreta segura
+const secret = crypto.randomBytes(64).toString('hex');
+
+// Configurar sessão
+app.use(session({
+    secret: secret, // Usar a chave secreta segura gerada
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hora
+}));
 
 // Conectando ao banco de dados
 connectToDatabase('Users');
@@ -70,39 +83,27 @@ app.post('/login', async (req, res) => {
             return res.status(400).send('Credenciais inválidas');
         }
 
-        // Simulação de geração de token
-        const token = generateToken(user);
+        // Armazenar informações do usuário na sessão
+        req.session.user = { id: user._id, name: user.name };
 
-        res.json({ token });
+        // Retornar uma mensagem de sucesso e a sessão ID
+        res.json({ message: 'Login bem-sucedido', sessionId: req.session.id });
     } catch (err) {
         res.status(500).send('Erro ao fazer login');
     }
 });
 
-// Simulação de geração de token
-function generateToken(user) {
-    // Gera um token de sessão com base no ID do usuário
-    return user._id.toString();
-}
-
 // Middleware de autenticação
-function verificarToken(req, res, next) {
-    const token = req.headers['authorization'];
-    if (!token) {
-        console.log('Token de autenticação não fornecido');
-        return res.status(401).send('Token de autenticação não fornecido');
+function verificarSessao(req, res, next) {
+    if (!req.session.user) {
+        return res.status(401).send('Não autenticado');
     }
-
-    // Simulação de verificação do token
-    const userId = token; // Considera-se que o token é o ID do usuário
-    req.usuario = { _id: userId };
     next();
 }
 
 // Rota protegida
-app.get('/rota-protegida', verificarToken, (req, res) => {
-    // Simulação de acesso à rota protegida
-    res.send(`Rota protegida acessada com sucesso pelo usuário com ID: ${req.usuario._id}`);
+app.get('/rota-protegida', verificarSessao, (req, res) => {
+    res.send(`Rota protegida acessada com sucesso por ${req.session.user.name}`);
 });
 
 // Iniciar o servidor
